d17ce52391006b67c8776b9f0e19f09f826aea13
pascal80000 <pascal80000@free.fr>
2023-01-04 16:56:03 +0100

    Mer 04 01 2023

--- maPremiereFenetre/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.afpa.mapremierefenetre {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.afpa.mapremierefenetre to javafx.fxml;
-    exports org.afpa.mapremierefenetre;
-}
