7a5e86387a6c07a224bfb385232fb24788bfef3e
pascal80000 <pascal80000@free.fr>
2023-06-16 12:08:30 +0200

    Ven 16 06 2023

--- 5_CDA/REACT/Exercice_Compteur/src/main.jsx
+++ 5_CDA/REACT/Exercice_Compteur/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client.js'
+import App2 from './Compteur.jsx'
+import './Compteur.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App2 />
+    
   </React.StrictMode>,
 )
